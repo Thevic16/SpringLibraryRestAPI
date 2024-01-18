337f037ac99d9242f1d3c1d09235532d6370ff96
Victor <vgomez@avathartech.com>
2024-01-18 10:05:58 -0400

    Upload Spring Inizializer data

---
+++ src/main/java/com/theitdojo/springlibraryrestapi/SpringLibraryRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.theitdojo.springlibraryrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryRestApiApplication.class, args);
+	}
+
+}
