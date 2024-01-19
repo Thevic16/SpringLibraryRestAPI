10d2a8b664366e14450d05dca9fc2d3aed8678d7
Victor <vgomez@avathartech.com>
2024-01-19 11:29:28 -0400

    Add JPA Repositories

---
+++ src/main/java/com/theitdojo/springlibraryrestapi/library/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.theitdojo.springlibraryrestapi.library;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findAllByAuthorByName(String name);
+}
---
+++ src/main/java/com/theitdojo/springlibraryrestapi/library/PageRepository.java
@@ -0,0 +1,6 @@
+package com.theitdojo.springlibraryrestapi.library;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PageRepository extends JpaRepository<Page, Long> {
+}
