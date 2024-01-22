79064b766d030867de1ca0bff55e706e5488e8cf
Victor <vgomez@avathartech.com>
2024-01-22 17:54:19 -0400

    Add pages data to the LibraryCommandLineRunner

--- src/main/java/com/theitdojo/springlibraryrestapi/library/BookRepository.java
+++ src/main/java/com/theitdojo/springlibraryrestapi/library/BookRepository.java
@@ -3,4 +3,5 @@ package com.theitdojo.springlibraryrestapi.library;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findBookByName(String name);
 }
--- src/main/java/com/theitdojo/springlibraryrestapi/library/LibraryCommandLineRunner.java
+++ src/main/java/com/theitdojo/springlibraryrestapi/library/LibraryCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.theitdojo.springlibraryrestapi.library;
 
+import com.theitdojo.springlibraryrestapi.utility.Utility;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -54,37 +55,64 @@ public class LibraryCommandLineRunner implements CommandLineRunner {
                 authorRepository.findAllByNameAndLastname("Jane", "Doe"),
                 genreRepository.findAllByName("Mystery"), null, LocalDate.parse("2023-05-15")));
 
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("The Enigma Code")));
+
         bookRepository.save(new Book("Echoes of Eternity",
                 authorRepository.findAllByNameAndLastname("John", "Smith"),
                 genreRepository.findAllByName("Science Fiction"), null, LocalDate.parse("2022-11-28")));
+
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Echoes of Eternity")));
 
         bookRepository.save(new Book("Whispers in the Wind",
                 authorRepository.findAllByNameAndLastname("Emily", "Johnson"),
                 genreRepository.findAllByName("Romance"), null, LocalDate.parse("2024-02-10")));
 
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Whispers in the Wind")));
+
         bookRepository.save(new Book("Lost in the Shadows",
                 authorRepository.findAllByNameAndLastname("Alex", "Turner"),
                 genreRepository.findAllByName("Thriller"), null, LocalDate.parse("2023-09-03")));
+
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Lost in the Shadows")));
 
         bookRepository.save(new Book("Beyond the Horizon",
                 authorRepository.findAllByNameAndLastname("Samantha", "Roberts"),
                 genreRepository.findAllByName("Adventure"), null, LocalDate.parse("2022-07-19")));
 
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Beyond the Horizon")));
+
         bookRepository.save(new Book("The Art of Deception",
                 authorRepository.findAllByNameAndLastname("Michael", "Anderson"),
                 genreRepository.findAllByName("Suspense"), null, LocalDate.parse("2023-12-05")));
+
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("The Art of Deception")));
 
         bookRepository.save(new Book("Serenade for the Stars",
                 authorRepository.findAllByNameAndLastname("Jessica", "Taylor"),
                 genreRepository.findAllByName("Fantasy"), null, LocalDate.parse("2024-01-22")));
 
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Serenade for the Stars")));
+
         bookRepository.save(new Book("Silent Echo",
                 authorRepository.findAllByNameAndLastname("Benjamin", "Williams"),
                 genreRepository.findAllByName("Mystery"), null, LocalDate.parse("2023-04-08")));
 
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("Silent Echo")));
+
         bookRepository.save(new Book("In the Line of Fire",
                 authorRepository.findAllByNameAndLastname("David", "Miller"),
                 genreRepository.findAllByName("Action"), null, LocalDate.parse("2024-03-11")));
+
+        pageRepository.save(new Page(1L, Utility.generateRandomString(10),
+                Utility.generateRandomString(100), bookRepository.findBookByName("In the Line of Fire")));
 
     }
 }
